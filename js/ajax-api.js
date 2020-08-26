e3ad6029adc7f8ee2b296d630d3067f8a4ea0d67
AlecTech <eremeevoleg@gmail.com>
2020-08-26 17:02:11 -0600

    Make data reflect inside Browser used for Of Loop to run through each object

--- js/ajax-api.js
+++ js/ajax-api.js
@@ -1,1 +1,14 @@
-fetch('https://www.dnd5eapi.co/api/spells').then(response => response.json()).then(data => {console.log(data);});
+fetch('https://www.dnd5eapi.co/api/spells').then(response => response.json()).then(data => {console.log(data);
+
+const results = data.results;
+console.log(results);
+
+const resultsUL = document.createElement('UL');
+for (const item of results )
+{
+    const itemLI = document.createElement('LI');
+    itemLI.textContent = `"Command is: "${item.index}" Name:   "${item.name} "Location of other API END POINTS:   "${item.url} !`;
+    resultsUL.appendChild(itemLI);
+}
+document.body.appendChild(resultsUL);
+});
